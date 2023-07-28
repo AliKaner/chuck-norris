72280bf1057480f1f51574472d49b80be6c42837
Ali Kaner <ali.kaner@hotmail.com>
2023-07-29 01:58:22 +0300

    fix: images strings

--- src/App.tsx
+++ src/App.tsx
@@ -14,10 +14,10 @@ async function getQuotes(): Promise<string | null> {
 }
 
 const imagesArray: string[] = [
-  'public/images/1.jpg',
-  'public/images/2.jpg',
-  'public/images/3.jpg',
-  'public/images/4.jpg',
+  'images/1.jpg',
+  'images/2.jpg',
+  'images/3.jpg',
+  'images/4.jpg',
 ];
 
 function getRandomImage(): string {
